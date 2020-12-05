4ea551c95b4be0cbf0e9f255555b8a4fd240a0c9
kevin trentini <trentinikevin6@gmail.com>
2020-12-05 11:01:17 +0100

    Add project files.

---
+++ Metodi/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Metodi
+{
+    public class Metodo
+    {
+        public static int InputInt()
+        {
+            int n;
+            do
+            {
+                Console.WriteLine("inserisci un numero");
+                n = Convert.ToInt32(Console.ReadLine());
+            } while (n < 0);
+            return n;
+        }
+        public static void Conta(int n1, int n2)
+        {
+            int pari = 0, dispari = 0;
+           
+            int max = Math.Max(n1, n2);
+            int min = Math.Min(n1, n2);
+            for (int count = min; count <= max; count++)
+            {
+                if (count % 2 != 0)
+                {
+                      dispari++;
+                    Console.WriteLine($"{count} dispari");
+                }
+                else
+                {
+                      pari++;
+                    Console.WriteLine($"{count} pari");
+                }
+                Console.WriteLine($"I positivi sono:{pari};I negativi sono:{dispari}");
+
+            }
+            
+        }
+    }
+}
---
+++ Prova/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Metodi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = Metodo.InputInt();
+            int n2 = Metodo.InputInt();
+            Metodo.Conta(n1, n2);
+        }
+    }
+}
